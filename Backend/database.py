d2a7a815e649a842259d6fbc78f3d1a7301c0cb1
SXPXL <your.email@example.com>
2025-12-28 18:26:55 +0530

    SSL error fixed/pre_ping=true

--- Backend/database.py
+++ Backend/database.py
@@ -5,7 +5,8 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,
+pool_pre_ping=True)
 
 def get_session():
     with Session(engine) as session:
